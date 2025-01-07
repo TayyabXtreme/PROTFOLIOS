008d83e02b994f1e65536974f4b65119ed7add5e
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-08 00:08:45 +0500

    page 4

---
+++ anzostudio/src/pages/Page4.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page4 = () => {
+  return (
+    <div className='h-screen p-4 bg-gray-50'>
+        <div className='h-full w-full bg-black rounded-[100px]'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Page4
